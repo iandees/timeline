f285d0d2bc116bba3a737b27e64b9dd9dab189d9
Ian Dees <ian.dees@gmail.com>
2024-12-18 20:13:32 -0600

    Install black as a dev dependency

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -6,12 +6,12 @@ from app import create_app
 @pytest.fixture
 def client():
     app = create_app()
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
 def test_hello(client):
-    rv = client.get('/')
+    rv = client.get("/")
     assert rv.status_code == 200
     assert b"Welcome to the Main Page" in rv.data
